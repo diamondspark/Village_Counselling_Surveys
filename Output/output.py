240ad3a88e4c11412c3e0be54600ea6f80f62cf0
Ubuntu <ubuntu@ip-172-31-42-63.us-east-2.compute.internal>
2018-08-04 17:56:08 +0000

    removed redo and redo_1 for ph1 and ph2

--- Output/output.py
+++ Output/output.py
@@ -17,9 +17,9 @@ class Output(object):
           self.df.drop(['user_1', 'email_1','age_1','email_1','gender_1','marital_status_1','sexual_orientation_1','professional_designation_1',
                         'years_in_practice_1'], axis=1, inplace=True)
           if self.phase =='1':
-               self.df.drop(['Phase2','Phase2_1','short','short_1'],axis=1,inplace=True)
+               self.df.drop(['Phase2','Phase2_1','short','short_1','redo','redo_1'],axis=1,inplace=True)
           elif self.phase =='2':
-               self.df.drop(['Phase1','Phase1_1','short','short_1'],axis=1,inplace=True)
+               self.df.drop(['Phase1','Phase1_1','short','short_1','redo','redo_1'],axis=1,inplace=True)
 
      def replace_nan_with_blank_string(self):
           self.df.replace('nan','',regex=True,inplace=True)
